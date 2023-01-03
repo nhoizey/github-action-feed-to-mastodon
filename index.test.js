3b707326b2e6319669f1793d35d723e0c2fb5f45
Nicolas Hoizey <nicolas@hoizey.com>
2023-01-03 23:09:59 +0100

    Auto update dependencies

--- index.test.js
+++ index.test.js
@@ -1,7 +1,3 @@
-const process = require("process");
-const cp = require("child_process");
-const path = require("path");
-
 test("nothing", async () => {
   expect(2).toBeGreaterThanOrEqual(1);
 });
